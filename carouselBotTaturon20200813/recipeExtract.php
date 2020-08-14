794a3a6c64b1254a41a42a2b0aeb1ca7d97644b5
Taturon <taturon5511@gmail.com>
2020-08-14 10:39:56 +0900

    [Update] ヒット件数が13件以上の場合にレシピをクイックリプライでランダムに表示されるように変更しました

--- carouselBotTaturon20200813/recipeExtract.php
+++ carouselBotTaturon20200813/recipeExtract.php
@@ -10,12 +10,26 @@ $stmt->execute();
 $ids = $stmt->fetchAll();
 
 // 検索ヒット件数が多い場合の処理
-// ヒット件数が13件より多い場合(クイックリプライの上限)
+// ヒット件数が13件(クイックリプライの上限)より多い場合
 if (count($ids) > 13) {
 	$rep = count($ids) . "件ヒットしました！";
-	$rep .= "\n対応するレシピが多すぎます！";
-	$rep .= "\nもう少し具体的に\n教えて欲しいです！";
+	$rep .= "\n対応するレシピが多すぎます...";
+	$rep .= "\n下記より選択されるか、";
+	$rep .= "\nもう少し具体的に教えて下さい！";
 	$reply['messages'][0]['text'] = $rep;
+	shuffle($ids);
+	$examples = array_rand($ids, 13);
+	$ids = array_intersect_key($ids, $examples);
+	foreach ($ids as $id) {
+		$reply['messages'][0]['quickReply']['items'][] = [
+			'type' => 'action',
+			'action' => [
+				'type' => 'message',
+				'label' => $id['category_name'],
+				'text' => $id['category_name']
+			]
+		];
+	}
 
 // ヒット件数が1件より多い場合
 } elseif (count($ids) > 1) {
