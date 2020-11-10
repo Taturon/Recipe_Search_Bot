fd617d2e30258fb5875b71ae078e7e92a0f74f55
Taturon <taturon5511@gmail.com>
2020-11-10 18:37:48 +0900

    [Add] buildディレクトリに設定ファイルを追加

---
+++ build/db_connect.php
@@ -0,0 +1,15 @@
+<?php
+// 定数定義ファイルの読み込み
+require_once('.env.php');
+
+// DB接続
+try {
+	$dbh = new PDO(PDO_DSN, USERNAME, PASSWORD, [
+		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+		PDO::ATTR_EMULATE_PREPARES => false,
+	]);
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+	exit('DB接続に失敗しました' . PHP_EOL . $e->getMessage());;
+}
