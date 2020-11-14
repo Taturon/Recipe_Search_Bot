393090620bbeda1a75579e8bfbd9076ae7536505
Taturon <taturon5511@gmail.com>
2020-11-14 16:04:13 +0900

    [Modify] SQL文のカラム名を修正しレシピ検索と単位換算の両方で使用する共通部分以外を削除

--- release/search_history_recording.php
+++ release/search_history_recording.php
@@ -3,8 +3,7 @@
 require_once('db_connect.php');
 
 $line_id = $event['source']['userId'];
-$category_name = $ids[0]['category_name'];
 $created_at = date('Y-m-d H:i:s');
-$sql = 'INSERT INTO `histories` (`line_id`, `category_name`, `created_at`) VALUES (?, ?, ?)';
+$sql = 'INSERT INTO `histories` (`line_id`, `word`, `created_at`) VALUES (?, ?, ?)';
 $stmt = $dbh->prepare($sql);
-$stmt->execute([$line_id, $category_name, $created_at]);
+$stmt->execute([$line_id, $word, $created_at]);
