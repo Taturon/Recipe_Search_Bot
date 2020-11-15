39760d6fe777488b29938abb6d6af0159803996c
Taturon <taturon5511@gmail.com>
2020-11-15 11:34:25 +0900

    [Modify] 検索履歴保存・更新判定時に有効期限を設定ファイルから呼び出す仕様に変更

--- release/search_history_recording.php
+++ release/search_history_recording.php
@@ -5,11 +5,12 @@ require_once('db_connect.php');
 $line_id = $event['source']['userId'];
 date_default_timezone_set('Asia/Tokyo');
 $created_at = date('Y-m-d H:i:s');
-$retention_period = date('Y-m-d H:i:s', strtotime('-7 day'));
+$retention_period = require_once('config.php');
+$retention_limit = date('Y-m-d H:i:s', strtotime('-' . $retention_period['retention_period'] . ' day'));
 
 $sql = 'SELECT `id` from `histories` WHERE `line_id` = ? AND `created_at` >= ? AND `word` = ? ORDER BY `id` DESC LIMIT 10';
 $stmt = $dbh->prepare($sql);
-$stmt->execute([$line_id, $retention_period, $word]);
+$stmt->execute([$line_id, $retention_limit, $word]);
 $id = $stmt->fetch();
 
 if ($id) {
