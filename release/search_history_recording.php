070e7acfe7593aa7feb2f5c6af9598ba5fe4e825
Taturon <taturon5511@gmail.com>
2020-11-15 10:59:22 +0900

    [Add] 入力された値が既に検索履歴にあった場合は挿入ではなくcreated_atの更新処理となる条件分岐を追加

--- release/search_history_recording.php
+++ release/search_history_recording.php
@@ -3,7 +3,21 @@
 require_once('db_connect.php');
 
 $line_id = $event['source']['userId'];
+date_default_timezone_set('Asia/Tokyo');
 $created_at = date('Y-m-d H:i:s');
-$sql = 'INSERT INTO `histories` (`line_id`, `word`, `created_at`) VALUES (?, ?, ?)';
+$retention_period = date('Y-m-d H:i:s', strtotime('-7 day'));
+
+$sql = 'SELECT `id` from `histories` WHERE `line_id` = ? AND `created_at` >= ? AND `word` = ? ORDER BY `id` DESC LIMIT 10';
 $stmt = $dbh->prepare($sql);
-$stmt->execute([$line_id, $word, $created_at]);
+$stmt->execute([$line_id, $retention_period, $word]);
+$id = $stmt->fetch();
+
+if ($id) {
+	$sql = 'UPDATE `histories` SET `created_at` = ? WHERE `id` = ?';
+	$stmt = $dbh->prepare($sql);
+	$stmt->execute([$created_at, $id['id']]);
+} else {
+	$sql = 'INSERT INTO `histories` (`line_id`, `word`, `created_at`) VALUES (?, ?, ?)';
+	$stmt = $dbh->prepare($sql);
+	$stmt->execute([$line_id, $word, $created_at]);
+}
