e49fe8d4d6653629d473c006b2def2b6b7a1b3b6
Taturon <taturon5511@gmail.com>
2020-11-22 18:56:18 +0900

    [Add] お店情報が取得できなかった場合のメッセージを設定するファイルを追加

---
+++ release/restaurant_search/no_result_text.php
@@ -0,0 +1,5 @@
+<?php
+$rep = 'すみません...' . PHP_EOL;
+$rep .= PHP_EOL . 'お近くにご紹介できる' . PHP_EOL . 'お店がございません...' . PHP_EOL;
+$rep .= PHP_EOL . '別の位置情報を' . PHP_EOL . 'お教え願います！';
+$reply['messages'][0] = ['type' => 'text', 'text' => $rep];
