8556cf9bd975926279d443d4998da88f40db328f
Taturon <taturon5511@gmail.com>
2020-11-23 16:59:07 +0900

    [Organize] search_histories.phpをhistoriesディレクトリ内へ移動

--- release/histories/search_histories.php
+++ release/histories/search_histories.php
@@ -4,7 +4,7 @@ require_once('db_connect.php');
 
 $line_id = $event['source']['userId'];
 date_default_timezone_set('Asia/Tokyo');
-$retention_period = require_once('config.php');
+$retention_period = require_once('../../config/retention_period.php');
 $retention_limit = date('Y-m-d H:i:s', strtotime('-' . $retention_period['retention_period'] . ' day'));
 $sql = 'SELECT `word` from `histories` WHERE `line_id` = ? AND created_at >= ? ORDER BY `created_at` DESC LIMIT 10';
 $stmt = $dbh->prepare($sql);
