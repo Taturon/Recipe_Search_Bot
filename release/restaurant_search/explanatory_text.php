b9ebcd8a1a2f9cea5d1e822b1317bf39c86c4344
Taturon <taturon5511@gmail.com>
2020-11-23 14:46:59 +0900

    [Modify] お店検索の説明テキストにユーザーの現在の設定値を表示する様に修正

--- release/restaurant_search/explanatory_text.php
+++ release/restaurant_search/explanatory_text.php
@@ -1,4 +1,12 @@
 <?php
+
+// ユーザーの設定値を取得
+require('restaurant_search/radius_search.php');
+$config = require_once('../config/radius.php');
+$radius = $config[$radius];
+
+// 返信用メッセージの設定
 $rep = '位置情報をお教え下されば';
-$rep .= PHP_EOL . '半径3km以内でご紹介できる' . PHP_EOL . 'お店をお伝え致します！';
+$rep .= PHP_EOL . '設定された半径内でご紹介できる' . PHP_EOL . 'お店をお伝え致します！' . PHP_EOL;
+$rep .= PHP_EOL . 'あなたの設定半径 : ' . $radius;
 $reply['messages'][0] = ['type' => 'text', 'text' => $rep];
