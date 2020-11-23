de3c4882dd0b96db3d5ee1c153183048c2da5ce3
Taturon <taturon5511@gmail.com>
2020-11-23 14:50:26 +0900

    [Modify] お店検索で検索結果がなかった場合のメッセージを修正

--- release/restaurant_search/no_result_text.php
+++ release/restaurant_search/no_result_text.php
@@ -1,5 +1,5 @@
 <?php
-$rep = 'すみません...' . PHP_EOL;
-$rep .= PHP_EOL . 'お近くにご紹介できる' . PHP_EOL . 'お店がございません...' . PHP_EOL;
-$rep .= PHP_EOL . '別の位置情報を' . PHP_EOL . 'お教え願います！';
+$rep = 'すみません!' . PHP_EOL . 'お近くにご紹介できる' . PHP_EOL . 'お店がございません...' . PHP_EOL;
+$rep .= PHP_EOL . '別の位置情報をお教え頂くか';
+$rep .= PHP_EOL . 'メニュー > 半径設定' . PHP_EOL . 'で検索半径を広げてみて下さい!';
 $reply['messages'][0] = ['type' => 'text', 'text' => $rep];
